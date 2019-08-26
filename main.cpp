edd5ef7ce12ff1ccda9c8f64f9daa99e90bcf021
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-26 18:35:09 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+#define MAX_SIZE 30
+
+class Queue{
+
+  public:
+    int A[MAX_SIZE];
+    int rear = -1
+    int front = -1;
+    
+  private:
+  
+    void enqueue(int data){
+      A[rear++] = data ;
+    }
+    
+    int dequeue(){
+      A[front++];
+    }
+    
+    int front(){
+      return A[front];
+    }
+    
+}
+
+
+
+int main(){
+
+}

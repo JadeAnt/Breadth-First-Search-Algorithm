e63d0673a4bc5db2c5a23ae38d6639a02fa15609
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-29 14:08:59 -0400

    Update C++_Implementation.cpp

--- C++_Implementation.cpp
+++ C++_Implementation.cpp
@@ -14,34 +14,12 @@ struct Vertex{
 class Queue{
 
   private:
-    int A[MAX_SIZE];
-    int rear = -1
-    int front = 0;
-    int queueSize = 0;
+    vector <int> A;
+    
   
   public:
   
-    void enqueue(Vertex data){
-      A[rear++] = data;
-      queueSize++;
-    }
-    
-    Vertex dequeue(){
-      data = A[front++];
-      queueSize--;
-      return data;
-    }
-    
-    Vertex front(){
-      return A[front];
-    }
-  
-    bool isEmpty(){
-       if(queueSize == 0)
-         return true;
-       else
-         return false;
-    }
+   
       
 }
 

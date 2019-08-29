59967803636b19abe41911b5123121fa9bc0d34d
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-29 14:11:36 -0400

    Update C_Implementation.c

--- C_Implementation.c
+++ C_Implementation.c
@@ -14,7 +14,32 @@ int adjMatrix[MAX][MAX];
 struct Vertex* listVerticies[MAX];
 
 //queue functions
+int A[MAX_SIZE];
+int rear = -1
+int front = 0;
+int queueSize = 0;
 
+void enqueue(Vertex data){
+  A[rear++] = data;
+  queueSize++;
+}
+    
+Vertex dequeue(){
+  data = A[front++];
+  queueSize--;
+  return data;
+}
+    
+Vertex front(){
+   return A[front];
+}
+
+bool isEmpty(){
+  if(queueSize == 0)
+     return true;
+  else
+     return false;
+}
 
 
 //graph functions

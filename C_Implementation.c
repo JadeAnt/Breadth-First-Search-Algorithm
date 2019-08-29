103d3922908f2ca9fc74ca5c0e9835135b1941bd
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-29 13:28:23 -0400

    Create C_Implementation.c

---
+++ C_Implementation.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_SIZE  30
+#define MAX 5
+
+struct Vertex{
+  char label;
+  bool visited;
+};
+
+int adjMatrix[MAX][MAX];
+struct Vertex* listVerticies[MAX];
+
+//queue functions
+
+
+
+//graph functions
+
+
+
+int main(){
+
+}

5c9366380c21771d7dcb68f7a371a308c54d2ab8
Jaden Antoine <jadenjantoine@gmail.com>
2019-08-31 12:13:57 -0400

    Update C_Implementation.c

--- C_Implementation.c
+++ C_Implementation.c
@@ -43,16 +43,67 @@ bool isEmpty(){
 int adjMatrix[MAX][MAX];
 struct Vertex* listVerticies[MAX];
 
-void addVertex(); //add vertex to vertex array
+void addVertex(char label){ //add vertex to vertex array
+  struct Vertex* vertex = (struct Vertex*) malloc(sizeof(struct Vertex));
+  vertex->label = label;
+  vertex->visited = false;
+  listVerticies[vertexCount++] = vertex;
+}
 
-void addEdge(); // add edge to edge array
+void addEdge(int start, int end){ // add edge to edge array
+  adjMatrix[start][end] = 1;
+  adjMatrix[start][end] = 1;
+}
 
-void displayVertex(); //displays know verticies
+void displayVertex(int vertexIndex){ //displays know verticies
+  printf("%c ", listVerticies[vertexIndex] -> label);
+}
 
-void adjUnvisitedVertex(); //shows any adjacent unvisited verticies
+void adjUnvisitedVertex(int vertexIndex){ //shows any adjacent unvisited verticies
+  int i;
+  
+  for(i = 0; i < vertexIndex; i++){
+      if(adjMatrix[vertexIndex][i] == 1 && listVerticies[i] -> visited == false)
+          return i;
+  }
+  
+  return -1;
+}
 
-void breadthFirstSearch(); //performs the breadthfirst search
-
+void breadthFirstSearch(){ //performs the breadthfirst search
+  int i;
+  
+  //mark first node as visited
+  listVerticies[0] -> visited = true;
+  
+  //display the vertex
+  displayVertex(0);
+  
+  //insert the vertex index in queue
+  enqueue(0);
+  int unvisitedVertex;
+  
+  while(!isEmpty()){
+    //get the unvisited vertex of the vertex at the front of the queue
+    int tempVertex = dequeue();
+    
+    //no adjacent vertex found
+    while( (unvisitedVertex = getadjUnvisitedVertex(tempVertex) ) != -1 ){
+      listVerticies[unvisitedVertex] -> visited = true;
+      displayVertex(unvisitedVertex);
+      enqueue(unvisitedVertex);
+    }
+  
+  }
+  
+  //queue is empty, search is completed, reset the visited flag for all nodes 
+  for(i = 0; i < vertexCount; i++){
+     listVerticies[i] -> visited = false;
+  }
+  
+  
+}
+  
 
 int main(){
 
